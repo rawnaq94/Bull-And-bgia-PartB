6f3dc6f12b8dc6ffee40aad9d5f8f4d961e1e0df
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-05-07 11:03:41 +0300

    Update SmartGuesser.hpp

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -2,6 +2,8 @@
 #include "calculate.hpp"
 #include <string>
 #include "Guesser.hpp"
+#include<iterator>
+#include <list>
 using std::string;
 
 namespace bullpgia
@@ -9,9 +11,22 @@ namespace bullpgia
     class SmartGuesser:public bullpgia::Guesser
     {
         public:
+        string first = ""; //first Guess
+        list <string> comb; //list of combinations
+        uint length;
+        int coun = 0; //counter of Guess
+        int num = 0; 
+        
 
-        void learn(string s)override;
+        void learn(string calculate)override;
         void startNewGame(uint length)override;
         string guess() override;   
+        
+        private:
+        void swap(char &a, char &b);
+        void combinations(string x, int y, int z);
+        
+       
+        
     }; 
 }
